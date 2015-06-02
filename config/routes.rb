73042a526ba119b36c3391a53a22f50e7cc70111
Gene Yoo <gyoo@Genes-MacBook-Pro.local>
2015-06-01 17:07:05 -0700

    Added comments functionality... still getting used to singular vs plural

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-Rails.application.routes.draw do
-  resources :articles
+Rails.application.routes.draw do  #take out do? or req for nested?
+  resources :articles do
+  	resources :comments
+  end
 
   root 'welcome#index'
 end
